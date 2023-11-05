a3b04cc57a71739f145dff7ce5db1cc8da8715aa
FouzanPS <mohammedfouzanmsd7@gmail.com>
2023-11-05 16:48:31 +0530

    some bug fixes

--- job appl/index.js
+++ job appl/index.js
@@ -2,7 +2,6 @@ document.getElementById("butn").addEventListener("click", function(event){
     event.preventDefault();
     update();
     clear();
-    alert("Form updated,Data stored locally on the browser");
 });
 
 function update() {
@@ -12,26 +11,23 @@ function update() {
     let position = document.getElementById("position").value;
     let skills = document.getElementById("skills").value;
 
-    let obj = {
-        name: name,
-        email: email,
-    };
 
-    if (phone.length !== 10) {
-        alert("Please enter a valid phone number!");
+    if (phone.length !== 10 || position === "" || skills === "") {
+        alert("Your phonerno is correct or positon or skills is empty please fill it properly.");
     } else {
+        let obj = {
+            name: name,
+            email: email,
+        };
         obj.phone = phone;
-    }
-
-    if (position === "" || skills === "") {
-        alert("Your position or skill column is empty, please fill it!");
-    } else {
         obj.position = position;
         obj.skills = skills;
+        let data = JSON.stringify(obj);
+        localStorage.setItem("FormData", data);
+        alert("Form updated,Data stored locally on the browser");
     }
 
-    let data = JSON.stringify(obj);
-    localStorage.setItem("FormData", data);
+
 }
 function clear(){
     document.getElementById("name").value = "";
